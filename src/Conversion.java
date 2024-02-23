a149bd1d425f6bed56234e531fbfb026675944d2
CptAhab1 <habbart01@gmail.com>
2024-02-23 16:13:33 -0600

    Comments and final edits

--- src/Conversion.java
+++ src/Conversion.java
@@ -1,13 +1,19 @@
+// This class contains several functions for converting temperatures
+// and displaying the results of the conversion.
 public class Conversion {
 
+    // Converts from Fahrenheit to Celsius
     public float convertFToC(float temp) {
         return ((float) 5 / 9) * (temp - 32);
     }
 
+    // Converts from Celsius to Fahrenheit
     public float convertCToF(float temp) {
         return (9 * temp / 5) + 32;
     }
 
+    // Displays the results from whichever conversion is processed,
+    // including a formatting instruction to round to 2 decimals
     public void displayResult(float temp, float tempConverted, char tempType) {
 
         switch (tempType) {
--- src/TempConverter.java
+++ src/TempConverter.java
@@ -1,13 +1,13 @@
 /*
 Alex Habbart
 0855541
-2/3/24
+2/23/24
 CSC 512
-Lab 2
+Lab 3
  */
 
-// This program will read input of temperature in either Fahrenheit or Celsius, then convert
-// that temperature into the other unit.
+// This program will read input of temperature in either Fahrenheit or Celsius, then
+// convert that temperature into the other unit.
 
 import java.util.Scanner;
 
@@ -17,29 +17,40 @@ public class TempConverter{
 
     public static void main(String[] args) {
 
+        // Intro message
         System.out.println("Welcome to \"WCUPA\" Weather Channel");
         System.out.println("Temperature Conversion Program \n");
 
+        // Scanner object declaration
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("\t[Q1] Enter a temperature in degrees (for example, 29.6): ");
 
+        // This will read in the temperature, without units
         float temp = scanner.nextFloat();
         scanner.nextLine();
 
         System.out.print("\t[Q2] Enter \'F\' (or \'f\') for Fahrenheit " +
                             "or \'C\' (or \'c\') for Celsius: ");
 
+        // This will allow the user to tell which unit they intend to have their
+        // entered temperature labelled as
         String tempTypeString = scanner.nextLine();
 
         System.out.println();
 
+        // Since there's no nextChar function, we read the temperature unit as a string
+        // then take the first char from that single-letter string
         char tempType = Character.toLowerCase(tempTypeString.charAt(0));
 
+        // declaring Conversion class object
         Conversion conversion = new Conversion();
 
+        // declaring the variables for the target temperature in the opposite unit
         float tempToC, tempToF;
 
+        // the conversion object contains the functions which convert the temp
+        // and output the results
         switch (tempType){
             case 'c':
                 conversion.displayResult(temp, conversion.convertCToF(temp), tempType);
@@ -48,6 +59,7 @@ public class TempConverter{
                 conversion.displayResult(temp, conversion.convertFToC(temp), tempType);
                 break;
             default:
+                // This will end the program if a unit besides C or F is entered
                 System.out.println("\n \tUnknown units -");
                 System.out.println("\tCannot do calculation -");
                 System.out.println("\tPlease enter either 'F' for Fahrenheit or " +
